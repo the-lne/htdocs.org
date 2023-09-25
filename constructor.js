bc38d82cdb41eb9261d03fc79fd537d1de09420b
admin <admin@htdocs.org>
2023-09-25 05:23:44 +0000

    cleaned up constructor function

--- constructor.js
+++ constructor.js
@@ -1,15 +1,36 @@
-function constructor() {
-	const width = ((Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)/100)*50); 
-	const height = ((Math.max(document.documentElement.clientWidth || 0, window.innerHeight || 0)/100)*40); 
+function setDivDimensions()
+{
+	/**
+	 * dynamically set div dimensions relative to browser dimensions
+	 */
 
-	var div = document.createElement("div");
-	div.style.width = '' + width + 'px';
-	div.style.height = '' + height + 'px';
-	div.style.color = "black";
+	const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0); 
+	const height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0); 
 
-	document.getElementById("canvasPage").appendChild(div);
+	var divCount = [ 
+		"canvasPage",	
+		"htDocs"	
+	];
+
+	var divWidthPercentage = [ 
+		50, // canvasPage
+		80	// htDocs
+	];
+	var divHeightPercentage = [ 
+		50, // canvasPage
+		80	// htDocs
+	];
+
+	for (var i = 0; i < divCount.length--; i++) 
+	{
+		var div = document.createElement("div");
+		div.style.width	 = '' + ((width/100)*divWidthPercentage[i]) + 'px';
+		div.style.height = '' + ((height/100)*divHeightPercentage[i]) + 'px';
+		document.getElementById(divCount[i]).appendChild(div);
+	}
 }
 
-function htDocfunction() {
-	alert(1);
+function constructor() 
+{
+	setDivDimensions();
 }
