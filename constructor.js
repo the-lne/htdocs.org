acb340d3d282477d4c3d2f16e1255aba78d42b2d
admin <admin@htdocs.org>
2023-09-26 09:01:51 +0000

    webgl here i come!

--- constructor.js
+++ constructor.js
@@ -1,36 +1,65 @@
-function setDivDimensions()
-{
+function iteratorPrototype() {
 	/**
-	 * dynamically set div dimensions relative to browser dimensions
+	 * dynamically set canvas dimensions relative to browser dimensions
 	 */
 
 	const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0); 
 	const height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0); 
 
-	var divCount = [ 
-		"canvasPage",	
-		"htDocs"	
+	var canvasList = [ 
+		/**
+		 * div_name,
+		 * another_div_name
+		 */
 	];
 
-	var divWidthPercentage = [ 
-		50, // canvasPage
-		80	// htDocs
+	var canvasWidthPercentage = [ 
+		50, // div_name
+		80	// another_div_name 
 	];
-	var divHeightPercentage = [ 
-		50, // canvasPage
-		80	// htDocs
+	var canvasHeightPercentage = [ 
+		50, // div_name
+		80	// another_div_name 
 	];
 
-	for (var i = 0; i < divCount.length--; i++) 
-	{
-		var div = document.createElement("div");
-		div.style.width	 = '' + ((width/100)*divWidthPercentage[i]) + 'px';
-		div.style.height = '' + ((height/100)*divHeightPercentage[i]) + 'px';
-		document.getElementById(divCount[i]).appendChild(div);
+	for (var i = 0; i < canvasList.length; i++) {
+		var canvas = document.createElement("canvas");
+		canvas.style.width	 = '' + ((width/100)*canvasWidthPercentage[i]) + 'px';
+		canvas.style.height = '' + ((height/100)*canvasHeightPercentage[i]) + 'px';
+		document.getElementById(canvasList[i]).appendChild(canvas);
 	}
 }
 
-function constructor() 
+function setCanvasDimensions() 
 {
-	setDivDimensions();
+	var canvas = document.querySelector("#easel");
+  	canvas.style.width ='60%';
+  	canvas.style.height='70%';
+  	canvas.width  = canvas.offsetWidth;
+  	canvas.height = canvas.offsetHeight;
+}
+
+function initWebGL() 
+{
+	const canvas = document.querySelector("#easel");
+	// Initialize the GL context
+	const gl = canvas.getContext("webgl");
+  
+	// Only continue if WebGL is available and working
+	if (!gl) {
+	  alert(
+		"Unable to initialize WebGL. Your browser or machine may not support it.",
+	  );
+	  return;
+	}
+
+	// Set clear color to black, fully opaque
+	gl.clearColor(1.0, 1.0, 1.0, 1.0);
+	// Clear the color buffer with specified clear color
+	gl.clear(gl.COLOR_BUFFER_BIT);
+} 
+
+function constructor() {
+	setCanvasDimensions();
+	initWebGL();
 }
