772d393c8637b928afd29463f6876418ef0582c7
root <root@htdocs.org>
2023-12-16 05:04:44 +0000

    saving before some changes

--- modules/spawner.js
+++ modules/spawner.js
@@ -5,22 +5,20 @@ class spawner {
         about:      "pages/about.html"
 	}
 	button = {
-		frontpage: 	document.getElementById("frontPage"),
-		download: 	document.getElementById("downloadsButton"),
+		frontpage: 	document.getElementById("frontpage"),
+		download: 	document.getElementById("downloads"),
 		about: 		document.getElementById("about")
 	}
 	newPage(page) {
 		document.getElementById("easel").src = page;
 	}
 	constructor() {
-		// these need to be turned into statically loaded content
 	    var canvas = document.querySelector("#easel");
-      	canvas.style.width  = '80%';
-      	canvas.style.height = '80%';
+      	canvas.style.width  = '85%';
+      	canvas.style.height = '85%';
       	canvas.width  = canvas.offsetWidth;
       	canvas.height = canvas.offsetHeight;
 	    canvas.style.borderRadius = "25px";
-		/////////////////////////////////////////////////////////
 
         this.button.frontpage.addEventListener('click', event => {
 			this.newPage(this.pages.frontpage);
